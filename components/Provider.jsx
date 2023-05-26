ac08fe69f8b7c38c0ce503944d94619a1d28ed5e
Prabin Nepal <prabinnepal1996@gmail.com>
2023-05-26 15:40:34 -0500

    Auth Providers

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+"use client";
 
-const Provider = () => {
+import { SessionProvider } from 'next-auth/react';
+
+const Provider = ({ children, session }) => {
   return (
-    <div>
-      <h1>Provider</h1>
-    </div>
+    <SessionProvider session={session}>
+        {children}
+    </SessionProvider>
   )
 }
 
